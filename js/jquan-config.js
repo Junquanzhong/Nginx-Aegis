bc449e79f80b1a334ffbd8a0045be2437bef473b
Junquanzhong <49361288+Junquanzhong@users.noreply.github.com>
2026-06-14 12:19:31 +0800

    Add files via upload

---
+++ js/jquan-config.js
@@ -0,0 +1,26 @@
+window.JQUAN_CONFIG =  {
+            theme: {
+                extend: {
+                    colors: {
+                        slate: {
+                            950: '#0b0f19'
+                        },
+                        gray: {
+                            100: '#f3f4f6',
+                            400: '#9ca3af',
+                            500: '#6b7280',
+                        }
+                    },
+                    backgroundImage: {
+                        'magic': 'linear-gradient(to right, #25aae1, #40e495, #30dd8a, #2bb673)',
+                    },
+                    boxShadow: {
+                        'glowing': '0 4px 15px 0 rgba(49, 196, 190, 0.75)',
+                    },
+                    fontFamily: {
+                        sans: ['Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'],
+                        mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', 'monospace']
+                    }
+                }
+            }
+        }
